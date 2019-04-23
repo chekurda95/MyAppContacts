a110a4cfbcf151d493f21805d16f68b095e26af5
Check <chekurda95@gmail.com>
2019-04-23 14:01:28 +0300

    new

--- app/src/main/java/com/example/myappcontacts/presentation/contacts/view/ContactsFragment.java
+++ app/src/main/java/com/example/myappcontacts/presentation/contacts/view/ContactsFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -93,7 +94,7 @@ public class ContactsFragment extends MvpAppCompatFragment implements IContactsV
         super.onViewCreated(view, savedInstanceState);
         setHasOptionsMenu(true);
         mUnbinder = ButterKnife.bind(this, view);
-
+        mTelNumberEditText.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
         mPhotoImage.setOnClickListener(v -> mContactsPresenter.onPhotoImageClicked());
     }
 
