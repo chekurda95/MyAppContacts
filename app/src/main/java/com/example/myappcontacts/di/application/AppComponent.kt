108ea4ebd222cd25e32092cbf12293fc9eaeb7b7
Check <chekurda95@gmail.com>
2019-04-28 21:34:09 +0300

    new Kotlin

--- app/src/main/java/com/example/myappcontacts/di/application/AppComponent.kt
+++ app/src/main/java/com/example/myappcontacts/di/application/AppComponent.kt
@@ -6,15 +6,12 @@ import com.example.myappcontacts.di.contactslist.ContactsListComponent
 import com.example.myappcontacts.di.contactslist.ContactsListModule
 import com.example.myappcontacts.di.contactsmap.ContactsMapComponent
 import com.example.myappcontacts.di.contactsmap.ContactsMapModule
-import com.example.myappcontacts.presentation.contacthost.view.ContactsHostActivity
 import dagger.Component
 import javax.inject.Singleton
 
 @Component(modules = [AppModule::class])
 @Singleton
 interface AppComponent {
-    fun inject(contactsHostActivity: ContactsHostActivity)
-
     fun plus(contactsModule: ContactsModule): ContactsComponent
 
     fun plus(contactsListModule: ContactsListModule): ContactsListComponent
